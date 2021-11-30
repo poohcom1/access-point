4265d14803bebe84fb6ae468630f718e1152f92c
poohcom1 <pooh677@gmail.com>
2021-11-30 19:48:49 +0700

    Stage 1 finished

--- ui/dialogue/parse_dialogues.py
+++ ui/dialogue/parse_dialogues.py
@@ -17,21 +17,11 @@ def parse(text):
     return text_json
 
 str = """
-Unknown: "sdkjfie...eelloa.... Hello! Hello! Check!",  "Is anyone on this comm?", "Can anyone hear me? Over"
-Mars: "This is agent 067-Mars with the Galactic Trade Task Force”, "Are you able to receive my transmission?"
-Unknown: "Oh thank god!", "Yes! Yes, we can pick up your transmission agent...", "This is radio operator...Aeonia from the North-Central command bunker", "So the GTTF picked up our distress call?", "We need backup ASAP"
-Mars: "Unfortunately, I am all there is", "This is a recon mission in order to verify the distress call. I need all the information you have", "Why was the call encrypted?"
-Aeonia: "...we were forced to encrypt our message in case it was intercepted", "As for other information, I don't have much", "No records of these creatures exist but they seem to be intelligent enough to be able to intercept and disrupt our comm systems both on and off-world" 
-Mars: "That explains why I lost contact with HQ...", "how are we communicating right now?"
-Aeonia: "We have been sending out messages on all frequencies regularly since main comms cut out after the first distress call", "You are the first response we have had back"
-Mars: "Ok", "You mentioned a base", "Are you able to track my position or guide me there?"
-Aeonia: "Negative", "We have no systems online that can track your position", "However, you should be able to follow the radar waypoints back to base", "That's how all our comms get transmitted on-world"
-Mars: "And just exactly how long do you think that might take?"
-Aeonia: "Anywhere between a few hours to a few days or weeks"
-Mars: "...okay then"
-
-
-
+Aeonia: "uoijf...hello test", "ah is this better now?"
+Mars: "Significantly", "The comms seem to be getting better"
+Aeonia: "Yes it seems so", "Surprisingly, your brute force approach to clearing out hostiles near radars seems to be working", "Don't know how though", "But the next area should be easier to handle now that the hostiles seem to be cleared out"
+Mars: "You're welcome by the way"
+Aeonia: "Not until you get to the command bunker"
 """
 
 
