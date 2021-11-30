cb0a5313200c5afb657e1f5466bf40d89c62097e
poohcom1 <pooh677@gmail.com>
2021-11-30 22:37:45 +0700

    Added triple modules and pickup

--- weapons/modules/Modules.gd
+++ weapons/modules/Modules.gd
@@ -1,10 +1,15 @@
 extends Node2D
 class_name Module
 
+const TYPE_ATTACK = 0
+const TYPE_DEFENSE = 1
+const TYPE_DASH = 2
+
 onready var player: KinematicBody2D = get_parent()
 
 export var ENERGY_COST := 1
 export var module_name: String
+export({Attack=TYPE_ATTACK, Defense=TYPE_DEFENSE, Dash=TYPE_DASH}) var TYPE: int
 
 var input: String
 var active = false
